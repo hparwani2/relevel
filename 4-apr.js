801ec2f180249a62312155f118f0c2025293c1af
Karan-Parwani-zs <karan.parwani@zopsmart.com>
2022-04-04 21:22:01 +0530

    4-apr-half

--- 4-apr.js
+++ 4-apr.js
@@ -1,9 +1,8 @@
-// second maximum salary.
+// // second maximum salary.
 let employees = [
     {
         name: 'babu',
         salary: 25000
-
     },
     {
         name: 'bahubali',
@@ -36,3 +35,20 @@ function swap(arr, i, j) {
     arr[i] = arr[j];
     arr[j] = temp;
 }
+
+// insertion sort
+function insertionSort(arr, n) {
+
+
+    for(let i=1;i<n;i++) {
+        let key = arr[i];
+        let j = i-1;
+        while(j>=0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+    return arr;
+}
+console.log(insertionSort([1,4,7,8,2], 5))
